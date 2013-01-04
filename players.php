524c0cd2cdf768be6da8a9ec7cc82a429dfc5fec
4a <furryforay@gmail.com>
2013-01-04 09:55:34 -0800

    Create players.php

---
+++ players.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+require('PHP/functionlist.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+<title>Fightan' /v/idya</title>
+<link rel="shortcut icon" href="FV.ico" >
+<link rel="stylesheet" type="text/css" href="CSS/newfightan.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+<style>
+#user
+{
+  display:block;
+  position:fixed;
+  top:15px;
+  right:30px;
+}
+#fvbux
+{
+  display:block;
+  text-align:center;
+}
+#create
+{
+  text-align:center;
+}
+</style>
+</head>
+<body>
+<?php
+include 'menu.php';
+?>
+<?php
+$IP = $_SERVER['REMOTE_ADDR'];
+$IP = ip2long($IP);
+if(isset($_SESSION['loggedin'])) {
+//        updatePoints(1, $_SESSION['name']);
+  $totalpoints = getPoints($_SESSION['name']);
+	echo "
+        <div id='user'>$_SESSION[name] ($totalpoints) <img src='IS/menu_open.png' /> | <a href='PHP/signout.php'>Log Out</a></div>
+        <div id='fvbux'>You have <br>
+        $totalpoints
+        <br>FVBux</div>
+        <div id='create'><a href='bets.php'>Create new match</a></div>
+        ";
+} else {
+	echo "<a href='signuppage.php'>Register</a><br/>";
+	echo "<a href='signinpage.php'>Log In</a>";
+}
+?>
+
+
+
+
+</body>
+</html>
