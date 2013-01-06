574e15e807af84018de8ae6dda36e6f5b1fa398f
4a <furryforay@gmail.com>
2013-01-05 19:04:50 -0800

    Update signinpage.php

--- signinpage.php
+++ signinpage.php
@@ -44,8 +44,8 @@ if(array_key_exists('submit',$_POST)) {
 <head>
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 <title>Fightan' /v/idya</title>
-<link rel="shortcut icon" href="../FV.ico" >
-<link rel="stylesheet" type="text/css" href="../CSS/newfightan.css">
+<link rel="shortcut icon" href="FV.ico" >
+<link rel="stylesheet" type="text/css" href="CSS/newfightan.css">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <style>
 #user
