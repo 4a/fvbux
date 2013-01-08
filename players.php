54f7c6e83f816f1fbdd86a16b6e51b2da027315e
4a <furryforay@gmail.com>
2013-01-08 04:27:24 -0800

    playing with profile layouts

--- players.php
+++ players.php
@@ -14,6 +14,7 @@ require('PHP/functionlist.php');
 #mainbox
 {
 max-width:100%;
+margin-top:30px;
 }
 #user
 {
@@ -22,34 +23,76 @@ max-width:100%;
   top:15px;
   right:30px;
 }
+
+#user-menu
+{
+float:left;
+display:inline-block;
+margin-right: 10px;
+text-align:right;
+}
+
+#user-avatar
+{
+display:block;
+float:right;
+}
+
 #content-left
 {
 position:absolute;
 top:0px;
 left:0px;
+z-index:10;
 }
 
 #avatar-border
 {
-width:82px;
-height:82px;
-border:1px solid #09f;
+width:202px;
+height:202px;
+border:3px solid #2F2F88;
 text-align: center;
 margin: auto;
+border-radius: 5px;
+background: #2F2F88;
+box-shadow: .2em .2em .3em rgba( 0, 0, 0, 0.45 );
 }
 
 #avatar-border img
 {
 margin: 1px;
+border-radius: 5px;
+}
+
+p
+{
+padding:10px;
 }
 
 #content-center
 {
 position:relative;
 top:0px;
-max-width:650px;
+max-width:595px;
 left:100px;
-padding-right:315px;
+margin-right:370px;
+background:#11183F;
+border-radius: 10px;
+box-shadow: .2em .2em .3em rgba( 0, 0, 0, 0.45 );
+z-index:0;
+}
+
+#content-title
+{
+/*background:#2F2F88;*/
+color:#fff;
+font-family: Trebuchet, arial, sans-serif;
+font-size: 25px;
+font-weight: normal;
+letter-spacing: .025em;
+line-height: 30px;
+margin: 0;
+padding: 0;
 }
 
 #content-right
@@ -58,8 +101,25 @@ float:right;
 position:absolute;
 top:0px;
 right:0px;
-max-width:200px;
+max-width:255px;
 }
+
+#leaderboard
+{
+width:255px;
+display:inline-block;
+background: rgba(0, 0, 0, 0.6);
+border-radius: 10px;
+}
+
+#matches
+{
+width:255px;
+display:inline-block;
+background: rgba(0, 0, 0, 0.6);
+border-radius: 10px;
+}
+
 </style>
 </head>
 <body>
@@ -78,19 +138,21 @@ if(isset($_GET['user']) and !empty($_GET['user'])) {
         if($row['username'] === $username) {
 	     $proGravatar = new TalkPHP_Gravatar();
 	     $proGravatar->setEmail($row['email']);
-	     $proGravatar->setSize(80);
+	     $proGravatar->setSize(200);
 	     $progravurl = $proGravatar->getAvatar();
 	     echo "<div id='content-left'>";
 
 	     /*avatar section*/
 	     echo "<div id='avatar-border'><img src='". $progravurl ."' alt='Gravatar' /></div>";
 	     if ($username === $_SESSION['name']) {echo "[<a href='http://en.gravatar.com/gravatars/new/'>Edit</a>]";}
+
+	     echo "<div id='content-title'>" . $username . "</div>";
 	     
 	     echo "</div>";
 	     
 	     echo "<div id='content-center'>";
-	     echo "<h1>" . $username . "</h1>";
-	     echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel sodales ipsum. Vestibulum id purus quam. Sed non ligula vitae est aliquet euismod. Vivamus semper dui congue metus fermentum volutpat. In nec justo risus, eu pretium neque. Quisque rhoncus mi dui. Morbi feugiat tempor est a posuere. Nulla elementum leo eu felis tincidunt non auctor tortor hendrerit. Duis accumsan porttitor ante at dapibus. Proin at turpis vitae erat aliquet accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent pharetra, enim vitae accumsan mattis, velit elit scelerisque nisl, vitae sollicitudin lorem libero id orci. Vivamus molestie massa non ante ullamcorper mollis. Phasellus pulvinar tortor quis nulla fringilla vestibulum.</p>
+
+	     echo "<p><img src='IS/wrap.gif' style='float:left;align:left;width:110px;height:185px' >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel sodales ipsum. Vestibulum id purus quam. Sed non ligula vitae est aliquet euismod. Vivamus semper dui congue metus fermentum volutpat. In nec justo risus, eu pretium neque. Quisque rhoncus mi dui. Morbi feugiat tempor est a posuere. Nulla elementum leo eu felis tincidunt non auctor tortor hendrerit. Duis accumsan porttitor ante at dapibus. Proin at turpis vitae erat aliquet accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent pharetra, enim vitae accumsan mattis, velit elit scelerisque nisl, vitae sollicitudin lorem libero id orci. Vivamus molestie massa non ante ullamcorper mollis. Phasellus pulvinar tortor quis nulla fringilla vestibulum.</p>
 
 <p>Curabitur auctor dolor nunc, et placerat tellus. In sed ipsum nec lorem pretium aliquam ut non ipsum. Nulla ut enim erat, posuere fermentum felis. Nunc gravida, urna eget dignissim egestas, nulla augue ultricies orci, vel viverra justo est quis dui. Donec ullamcorper dolor in odio dapibus euismod. Proin et dolor dui. Suspendisse in lobortis est. In cursus ante quis purus pulvinar eget condimentum dolor varius. Phasellus a velit nisi, et pellentesque tellus. Pellentesque sed erat eget ipsum vestibulum iaculis sed ut arcu. Nunc aliquet dignissim justo, quis scelerisque urna vestibulum vel. Praesent nisi dui, scelerisque ac feugiat at, vehicula sit amet mi. Quisque ac ullamcorper lacus. Duis ut sapien nec sem tristique ullamcorper. Nunc sed magna ut felis ornare pellentesque molestie iaculis purus.</p>
 
@@ -120,7 +182,7 @@ if(isset($_SESSION['loggedin'])) {
 }
 }
 
-echo "<div id='content-right'><div id='leaderboard' style='display:inline-block;background-color:black'>leaedr board<br>";
+echo "<div id='content-right'><p><div id='leaderboard'>leaedr board<br>";
 
 	if(!($stmt = $mysqli->prepare("SELECT username, points FROM user ORDER BY points DESC LIMIT 10"))) {
 		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
@@ -136,9 +198,9 @@ echo "<div id='content-right'><div id='leaderboard' style='display:inline-block;
         echo $i++ .". <a href='players.php?user=". $leadname ."'>". $leadname ."</a> (". $leadpoints .")<br>";
     }
     
-echo "</div>
-
-<div id='matches' style='display:inline-block;background-color:black'>stuff to bet on | <a href='bets.php'>Create new match</a><br>";
+echo "</div></p>
+<p>
+<div id='matches'>stuff to bet on | <a href='bets.php'>Create new match</a><br>";
 
 	if(!($stmt = $mysqli->prepare("SELECT `ID`, `Input 1`, `Input 2`, `Mod` FROM `bets_matches` ORDER BY `ID` DESC LIMIT 50"))) {
 		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
@@ -154,7 +216,7 @@ echo "</div>
         echo $i++ .". <a href='bets.php?mid=". $matchNo ."'> ". $name1 ." VS. ". $name2 ."</a> [Moderator: ". $mod ."]<br>";
     }
 
-echo "</div></div>";
+echo "</div></p></div>";
 ?>
 </div>
 </body>
