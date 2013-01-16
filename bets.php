c0c86b648eb09a5ff1c5555faf3fa323093c4330
4a <furryforay@gmail.com>
2013-01-16 02:41:40 -0800

    bets.php - Added a list of public open bets

--- bets.php
+++ bets.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 require('PHP/functionlist.php');
-/*
-	 Need to add if the user has logged in before getting session variables
-*/
-$totalpoints = getPoints($_SESSION['name']);
+if(isset($_SESSION['loggedin'])) {
+$totalpoints = getPoints($_SESSION['name']);  }
 $IP = $_SERVER['REMOTE_ADDR'];
 $IP = ip2long($IP);
 if(array_key_exists('submit',$_POST)) {
@@ -26,15 +24,29 @@ if(array_key_exists('submit',$_POST)) {
 body
 {
   text-align:center;
-}
-
+}  
 #user
 {
   display:block;
-  position:fixed;
+  position:absolute;
   top:15px;
   right:30px;
 }
+
+#user-menu
+{
+float:left;
+display:inline-block;
+margin-right: 10px;
+text-align:right;
+}
+
+#user-avatar
+{
+display:block;
+float:right;
+}
+
 #fvbux
 {
   display:block;
@@ -53,7 +65,7 @@ include 'user.php';
 ?>
 <?php
 /* $IPBYPASS is used to get around the ip check so you are able to test bet creation locally */
-$IPBYPASS = FALSE;
+$IPBYPASS = TRUE;
 
 if(isset($_SESSION['loggedin'])) 
 {
@@ -63,14 +75,14 @@ if(isset($_SESSION['loggedin']))
   $match_ID = $_GET['mid'];
   $match_ID = $mysqli->real_escape_string($match_ID); 
   $Match = new MatchInfo($match_ID);
-  $matchid = $Match->getMatchID();             
+  $matchid = $Match->getMatchID();
   $input1 = $Match->getInput1();
   $input2 = $Match->getInput2();
   $mod = $Match->getMod();
   $modip = $Match->getModIP();
-  if($matchid == $match_ID) 
+  if($matchid == $match_ID)
   {
-   if ($_SESSION['name'] === $mod) 
+   if ($_SESSION['name'] === $mod)
    {
    echo "<h1>" . $input1 . "</h1>";
    echo "<h4>VS</h4>";
@@ -90,6 +102,25 @@ if(isset($_SESSION['loggedin']))
     echo "<h4>VS</h4>";
     echo "<h1>" . $input2 . "</h1>";
     echo "Moderator: <a href='players.php?user=" . $mod . "'>" . $mod . "</a>";
+
+/* Begin list of open bets */  
+    echo "<br><br><div id='open_bets'>Open Bets:<br>";
+
+    $stmt = $mysqli->prepare("SELECT `ID`, `username 1`, `value`, `user1choice` FROM `bets_money` WHERE (`match`=? AND `status`='open' AND `private`=0) ");
+    $stmt->bind_param("s", $match_ID);
+    $stmt->execute();
+    $stmt->bind_result($betNo, $user1name, $betvalue, $user1choice);
+    $i = 1;
+
+    while ($stmt->fetch())
+    {
+    echo $i++ .". <a href='playerbet.php?bid=". $betNo ."'> ". $betvalue ." FVBux on ". $user1choice ."</a>
+         [<a href='players.php?user=". $user1name ."'>". $user1name ."</a>]<br>";
+    }
+
+    echo "</div>";
+/* End list of open bets */
+
     echo "<br />Select a bet from the list or"
     ."<br /><a href='createbet.php?mid=$match_ID'><img src='IS/createbet.jpg'></a>";
    }
@@ -107,13 +138,16 @@ if(isset($_SESSION['loggedin']))
  else 
  {
   echo "
+  This is the submission form for creating and moderating a match-up.
+  As the moderator, you cannot bet on an outcome;
+  however, you will have the opportunity to earn fvbux based on the amount of people who have participated in your match-up.
   <form action='$_SERVER[PHP_SELF]' method='post'>
   <input type='text' name='input1'><br />
   VS<br />
   <input type='text' name='input2'><br />
   <input type='submit' name='submit' value='Submit' />";
  }
-} 
+}
 else 
 {
  echo "<a href='signuppage.php'>Register</a><br/>";
