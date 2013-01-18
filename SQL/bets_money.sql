b04e2aa0490ba8e58a56470913c792b3ba4b6d5b
foray4a <furryforay@gmail.com>
2013-01-17 18:43:51 -0800

    Added winner column

--- SQL/bets_money.sql
+++ SQL/bets_money.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
+-- version 3.3.9
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jan 09, 2013 at 06:49 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Host: localhost
+-- Generation Time: Jan 18, 2013 at 03:43 AM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -37,19 +36,20 @@ CREATE TABLE IF NOT EXISTS `bets_money` (
   `status` enum('open','locked','closed') NOT NULL,
   `private` tinyint(1) NOT NULL,
   `user1choice` varchar(255) NOT NULL,
+  `winner` varchar(16) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `bets_money`
 --
 
-INSERT INTO `bets_money` (`ID`, `match`, `username 1`, `username 2`, `value`, `odds`, `IP`, `status`, `private`, `user1choice`) VALUES
-(1, 0, '', '', 1, 1.500, 0, 'open', 0, ''),
-(2, 1, 'foray', '', 500, 1.000, 2130706433, 'open', 0, ''),
-(6, 1, 'foray', '', 500, 1.000, 2130706433, 'open', 0, ''),
-(9, 1, 'joe', '', 21, 1.000, 2130706433, 'open', 1, 'daygo');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `bets_money` (`ID`, `match`, `username 1`, `username 2`, `value`, `odds`, `IP`, `status`, `private`, `user1choice`, `winner`) VALUES
+(1, 0, '', '', 1, 1.500, 0, 'open', 0, '', ''),
+(2, 1, 'foray', '4a4a', 500, 1.000, 2130706433, 'locked', 0, '', ''),
+(6, 1, 'foray', '', 500, 1.000, 2130706433, 'open', 0, '', ''),
+(9, 1, 'joe', '', 21, 1.000, 2130706433, 'open', 1, 'daygo', ''),
+(10, 1, '4a4a', '', 1009, 1.000, 2130706433, 'open', 0, 'maygo', ''),
+(11, 25, '4a4a', '', 8, 1.000, 2130706433, 'open', 0, 'jwong', ''),
+(12, 25, '4a4a', '', 6, 1.000, 2130706433, 'open', 1, 'daigo', ''),
+(13, 1, '4a4a', '', 500, 1.000, 2130706433, 'open', 0, 'daygo', '');
