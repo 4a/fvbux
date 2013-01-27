9d2a52842804774d49d1dddc93bb342b65d3ff69
Joe Staines <stickystaines@gmail.com>
2013-01-27 21:33:39 +0000

    Updated adminpanel to change any users' points

--- adminpanel.php
+++ adminpanel.php
@@ -1,7 +1,43 @@
 <?php
 session_start();
+require('PHP/connect.php');
+require('PHP/functionlist.php');
 if($_SESSION['level'] != 'admin') {
 	header('Location: players.php');
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if(!empty($_POST['changepoints-submit'])) {
+
+		if(empty($_POST['addorminus'])) {
+			$errmsg = "Add or minus not selected";
+		}
+	
+		if(empty($_POST['amount'])) {
+			$errmsg = "Amount value not given";
+		}
+	
+		if(!preg_match('/^\d+$/', $_POST['amount'])) {
+			$errmsg = "Amount is not numeric value";
+		}
+
+		if(empty($_POST['username'])) {
+			$errmsg = "Username not entered";
+		} else {
+			if(!doesUserExist($_POST['username'])) {
+				$errmsg = "Username entered does not exist";
+			}
+		}
+		
+		if(!isset($errmsg)) {
+			if($_POST['addorminus'] == "add") {
+				updatePoints($_POST['amount'], $_POST['username']);
+			} else {
+				minusPoints($_POST['amount'], $_POST['username']);
+			}
+		}
+	}
+
 }
 
 
@@ -16,17 +52,37 @@ if($_SESSION['level'] != 'admin') {
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="JS/idtabs.js"></script>
 <style>
+#user
+{
+  display:block;
+  position:absolute;
+  top:15px;
+  right:30px;
+  line-height:22px;
+}
+
+#error
+{
+	color: red;
+}
+</style>
 </head>
 
 <body>
-
+<?php
+include('menu.php');
+include('user.php');
+?>
+<?php if(isset($errmsg)) echo "<div id='error'>" . $errmsg . "</div>"; ?>
 <div id='changeuserpoints'>
-<h4>Change User's Points</h4>
-<form>
-Username: <input type='text' name='username' />
-Add/Minus: <input type='radio' name='addorminus' />&nbsp;<input type='radio' name='addorminus' />
-Amount: <input type='text' name='amount' />
+<h2>Change User's Points</h2>
+<form name='changepoints' action='<?php echo $_SERVER['PHP_SELF']; ?>' method='POST'>
+Username: <input type='text' name='username' /><br />
+Add/Minus: Add<input type='radio' name='addorminus' value='add'/>&nbsp;Minus<input type='radio' name='addorminus' value='minus'/><br />
+Amount: <input type='text' name='amount' /><br/>
+<input type='submit' name='changepoints-submit' />
 </form>	
 </div>
 
 </body>
+</html>
