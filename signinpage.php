9715ab3d443fdc27933ac223c76cd3af0c533d8a
4a <furryforay@gmail.com>
2013-01-31 15:58:37 -0800

    gave signinpage a makeover

--- signinpage.php
+++ signinpage.php
@@ -48,22 +48,52 @@ if(array_key_exists('submit',$_POST)) {
 <link rel="stylesheet" type="text/css" href="CSS/newfightans.css">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
 <style>
-#user
+.loginbox
 {
-  display:block;
-  position:fixed;
-  top:15px;
-  right:30px;
+position:relative;
+height:384px;
+width:640px;
+margin:auto;
+margin-top:40px;
+background:#11183F;
+border:1px solid #fff;
 }
-#fvbux
+
+.signin
 {
-  display:block;
-  text-align:center;
+width:300px;
+height:382px;
+display:inline-block;
+position:relative;
+padding:0px 16px;
 }
-#create
+
+.separator
 {
-  text-align:center;
+width:1px;
+height:352px;
+background:#fff;
+display:inline-block;
+position:absolute;
+left:50%;
+top:17px;
 }
+
+.register
+{
+width:300px;
+height:382px;
+display:inline;
+position:absolute;
+left:50%;
+padding:0px 16px;
+}
+
+.textinput
+{
+width:280px;
+}
+
 </style>
 </head>
 <body>
@@ -71,10 +101,27 @@ if(array_key_exists('submit',$_POST)) {
 include 'menu.php';
 ?>
 <?php if($errlogin != "") { echo "<div style='color:red'>$errlogin</div><br />"; } ?>
-Sign in:
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
-Username: <input type="text" name="username" ><br />
-Password: <input type="password" name="password" ><br />
-<input type="submit" name="submit" value="Submit" />
+<div class="loginbox">
+
+<div class='signin'>
+	<h2>Sign in</h2>
+	
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method='post'>
+	<div>Username</div>
+	<input type="text" name="username" class="textinput"><br />
+
+	<div style="margin-top:16px">Password</div><input type="password" name="password" class="textinput"><br />
+	<input class='loginsubmit' type="submit" name="submit" value="Sign In" />
+</div>
+
+<div class='separator'></div>
+	
+<div class='register'>
+	<h2>Register</h2>
+	<p>Don't have an account?</p>
+	<p><a href='signuppage.php'>Register</a> a new account</p>
+</div>
+		
+</div>	
 </body>
 </html>
