68306e3d9784c3c97bd98dd51da65781bbffc344
4a <furryforay@gmail.com>
2013-01-31 14:11:04 -0800

    tried to make it look like the drawing

--- testpage.php
+++ testpage.php
@@ -27,7 +27,7 @@ require('PHP/functionlist.php');
 	width:496px;
 }
 
-.match
+.matchContainer
 {
 	height: 46px;
 	width: 100%;
@@ -39,13 +39,27 @@ require('PHP/functionlist.php');
 	padding: 4px 4px 4px 4px;
 }
 
+.match
+{
+	width:50%;
+	float:left;
+}
+
+/* since the widths of these divs is 50% adding a border between the match div and the info div will create a line break. if you want a border it would be better to set the widths to a pixel value that is half of the width of the container minus the width of the border you want. */
+
+.matchInfo
+{
+	width:50%;
+	float:right;
+}
+	
 .match img {
 	float: left;
 	width: 45px;
 	height: 45px;
 }
 
-.match:hover
+.matchContainer:hover
 {
 	background-color: #297EFF;
 	color: white;
@@ -63,12 +77,26 @@ $MatchImage->setSize(45);
 $imgURL = $MatchImage->getAvatar();
 ?>
 <div id='matchesBox'>
-	<div class='match'>
-	<img src='<?php echo $imgURL; ?>' alt='pic' >
+	<div class='matchContainer'>
+		<div class='match'>
+		<img src='<?php echo $imgURL; ?>' alt='pic' >
+		<div>Evo</div>
+		<div>Bob vs Jim</div>
+		</div>
+		
+		<div class='matchInfo'>
+		<div>Mod: Joe</div>
+		<div>Info: Losers Bracket Semi-Finals</div>
+		</div>
 	</div>
-	<div class='match'>
-	Hiya
-	</div>
+	<div class='matchContainer'>
+		<div class='match'>
+		Hiya
+		</div>
+		
+		<div class='matchInfo'>
+		</div>
+	</div>	
 </div>
 </body>
 
